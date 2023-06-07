3e87677ad67992f00ee588ee06bcff0946957efe
radichenko <radichenkonazar@gmail.com>
2023-06-07 17:58:14 +0300

    Created grid for maze

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const step = 25;
 const borderWidth = 2;
 const mazeHeight = 200;
 const mazeWidth = 300;
+
+const my = mazeHeight / step;
+const mx = mazeWidth / step;
+
+const grid = [];
+for (let i = 0; i < my; i++) {
+	const rowArray = [];
+	for (let a = 0; a < mx; a++) {
+		sg.push({ u: 0, d: 0, l: 0, r: 0, v: 0 });
+	}
+	grid.push(rowArray);
+}
