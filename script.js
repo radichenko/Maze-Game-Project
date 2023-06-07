08ffb0cad614a7a3433bc87a3ed2287806364c6d
radichenko <radichenkonazar@gmail.com>
2023-06-07 21:45:47 +0300

    Implemented animated key styling for movement controls

--- script.js
+++ script.js
@@ -304,3 +304,40 @@ function limitedShuffle(array, limit) {
 	let combined = constrained.concat(remaining);
 	return combined;
 }
+
+function animKeys(key) {
+	if (key.id === "bUp") {
+		key.style.border = "3px #fff solid";
+		key.style.borderTop = "1px #fff solid";
+		key.style.borderBottom = "4px #fff solid";
+		key.style.transform = "translateY(-2px)";
+	}
+	if (key.id === "bDown") {
+		key.style.border = "3px #fff solid";
+		key.style.borderBottom = "1px #fff solid";
+		key.style.borderTop = "4px #fff solid";
+		key.style.transform = "translateY(2px)";
+	}
+	if (key.id === "bLeft") {
+		key.style.border = "3px #fff solid";
+		key.style.borderLeft = "1px #fff solid";
+		key.style.borderRight = "4px #fff solid";
+		key.style.transform = "translateX(-2px)";
+	}
+	if (key.id === "bRight") {
+		key.style.border = "3px #fff solid";
+		key.style.borderRight = "1px #fff solid";
+		key.style.borderLeft = "4px #fff solid";
+		key.style.transform = "translateX(2px)";
+	}
+
+	setTimeout(() => {
+		key.style.border = "2px #fff solid";
+		key.style.borderTop = "2px #fff solid";
+		key.style.borderBottom = "2px #fff solid";
+		key.style.borderLeft = "2px #fff solid";
+		key.style.borderRight = "2px #fff solid";
+		key.style.transform = "translateY(0px)";
+		key.style.transform = "translateX(0px)";
+	}, "150");
+}
