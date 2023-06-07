0fa38d27033414dfda04609b2a9b898e435b0074
radichenko <radichenkonazar@gmail.com>
2023-06-07 17:39:13 +0300

    Initialized variables with DOM element references

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.getElementById("container");
+const maze = document.getElementById("maze");
+const player = document.getElementById("player");
+const home = document.getElementById("home");
+const emotion = document.getElementById("emotion-icon");
+
+const bUp = document.getElementById("button-up");
+const bDown = document.getElementById("button-down");
+const bLeft = document.getElementById("button-left");
+const bRight = document.getElementById("button-right");
+
+const step = 25;
+const borderWidth = 2;
+const mazeHeight = 200;
+const mazeWidth = 300;
